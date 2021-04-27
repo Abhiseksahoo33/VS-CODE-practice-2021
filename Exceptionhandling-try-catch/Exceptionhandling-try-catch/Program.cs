3ac01d35d6c9d28a52bca62040c2979a300d4fe7
abhisek <abhiseksahoo33@gmail.com>
2021-04-27 22:11:50 +0530

    try-catch-ExceptionHandling

---
+++ Exceptionhandling-try-catch/Exceptionhandling-try-catch/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptionhandling_try_catch
+{ 
+  class Demo
+{
+    public void check(int n)
+
+    {
+        if (n < 0)
+        {
+            throw new DivideByZeroException("Negative number not allowed ..");
+        }
+        else if (n == 0)
+        {
+            throw new IndexOutOfRangeException("Zero is invalid ..");
+        }
+        else
+        {
+            Console.WriteLine("N's value is :  " + n);
+        }
+    }
+    static void Main(string[] args)
+    {
+        int n;
+        Console.WriteLine("Enter the value of N : ");
+        n = Convert.ToInt32(Console.ReadLine());
+
+        Demo obj = new Demo();
+
+        try
+        {
+            obj.check(n);
+        }
+        catch (DivideByZeroException e)
+
+        {
+
+            Console.WriteLine(e.Message);
+        }
+        catch (IndexOutOfRangeException e)
+
+        {
+
+            Console.WriteLine(e.Message);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
+}
