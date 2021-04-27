f2acb192f82912deda3bc9b0f8455396e982ccc3
abhisek <abhiseksahoo33@gmail.com>
2021-04-27 21:19:05 +0530

    db connection-with try catch finally example

--- empdatabase/empdatabase/Program.cs
+++ empdatabase/empdatabase/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,29 @@ namespace empdatabase
     {
         static void Main(string[] args)
         {
+            SqlConnection conn=null;
+
+            try
+            {
+                conn = new SqlConnection("integrated security=true; data source=ABHI-SEK\MSSQLSERVER01,initial catalog=Empdb");
+                conn.Close();
+                Console.WriteLine("CONNECTED .....");
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e); ;
+            }
+            finally
+                {
+                if(conn!=null)
+                {
+                    conn.Close();
+                }
+
+
+            }
         }
     }
 }
