5ccf79742ecc996d434936f7c90ed7505dbf4f05
abhisek <abhiseksahoo33@gmail.com>
2021-06-06 22:08:43 +0530

    Employee crud Demo Web App

---
+++ Employee_CRUD_MVC_WebApp/Employee_CRUD_MVC_WebApp/Controllers/EmpController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Employee_CRUD_MVC_WebApp.Models;
+
+namespace Employee_CRUD_MVC_WebApp.Controllers
+{
+    public class EmpController : Controller
+    {
+        private sqlpracticeEntities db = new sqlpracticeEntities();
+
+        // GET: Emp
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Emps.ToListAsync());
+        }
+
+        // GET: Emp/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Emp emp = await db.Emps.FindAsync(id);
+            if (emp == null)
+            {
+                return HttpNotFound();
+            }
+            return View(emp);
+        }
+
+        // GET: Emp/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Emp/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Empno,name,dept,desig,basic")] Emp emp)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Emps.Add(emp);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(emp);
+        }
+
+        // GET: Emp/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Emp emp = await db.Emps.FindAsync(id);
+            if (emp == null)
+            {
+                return HttpNotFound();
+            }
+            return View(emp);
+        }
+
+        // POST: Emp/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Empno,name,dept,desig,basic")] Emp emp)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(emp).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(emp);
+        }
+
+        // GET: Emp/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Emp emp = await db.Emps.FindAsync(id);
+            if (emp == null)
+            {
+                return HttpNotFound();
+            }
+            return View(emp);
+        }
+
+        // POST: Emp/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Emp emp = await db.Emps.FindAsync(id);
+            db.Emps.Remove(emp);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Employee_CRUD_MVC_WebApp/Employee_CRUD_MVC_WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Employee_CRUD_MVC_WebApp.MvcApplication" Language="C#" %>
